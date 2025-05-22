6ed1d97c22418f419debd0062d12835712119dc3
wardahrehman <wardahrehman703@gmail.com>
2025-05-22 13:25:24 +0500

    build

--- src/pages/resourcelanding.js
+++ src/pages/resourcelanding.js
@@ -44,7 +44,9 @@ const Resourcelanding = () => {
                     <div className='blogdetail new_details_pagee'>
                         <div className='parenttext newtext'>
                             <div className='twicebtn'>
+                                     <Link href="/?section=services">
                                 <p>Services</p>
+                                </Link>
                                 <svg xmlns="http://www.w3.org/2000/svg" width="14" height="15" viewBox="0 0 14 15" fill="none">
                                     <path d="M5.25 11L8.75 7.5L5.25 4" stroke="#9D9D9D" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
                                 </svg>
