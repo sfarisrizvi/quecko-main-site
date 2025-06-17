1dedc598b5872f29a55869144984043b9c325dba
mirbasit01 <mirt11477@gmail.com>
2025-06-17 18:48:15 +0500

    get in touch

---
+++ src/pages/GetinTouch.js
@@ -0,0 +1,20 @@
+import React from 'react'
+ 
+const Getintouch = () => {
+    return (
+<>
+
+ <div className='forms_div_section'>
+                            <div className='inputttunner'>
+                                <input placeholder='Name' type="text" id="fname" name="fname" />
+                                <input placeholder='Email@company.com' type="text" id="fname" name="fname" />
+                            </div>
+                            <input placeholder='Telegram' type="text" id="fname" name="fname" />
+                            <textarea placeholder='Your Message' id="w3review" name="w3review" rows="4" cols="50" />
+                            <button>Get in Touch</button>
+                        </div>
+</>
+          )
+}
+
+export default Getintouch
