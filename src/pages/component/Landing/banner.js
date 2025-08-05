95eef67ed398082a62db276943847d3987e865c8
hamzach625 <hamzach625@gmail.com>
2025-08-05 20:10:12 +0500

    chnages

--- src/pages/component/Landing/banner.js
+++ src/pages/component/Landing/banner.js
@@ -10,34 +10,34 @@ const Banner = () => {
   const textRef = useRef(null);
 
   useEffect(() => {
-  if (window.innerWidth <= 768) return;
+    if (!textRef.current) return;
 
-  if (!textRef.current) return;
+    const element = textRef.current;
+    const text = element.innerText;
 
-  const element = textRef.current;
-  const text = element.innerText;
+    element.innerHTML = text
+      .split("")
+      .map(
+        (char) =>
+          `<span class="char">${char === " " ? "&nbsp;" : char}</span>`
+      )
+      .join("");
 
-  element.innerHTML = text
-    .split("")
-    .map(
-      (char) =>
-        `<span class="char">${char === " " ? "&nbsp;" : char}</span>`
-    )
-    .join("");
 
-  gsap.timeline()
-    .set(".style-1 .char", { opacity: 0, y: 50 })
-    .to(".style-1 .char", {
-      y: 0,
-      opacity: 1,
-      duration: 1.8,
-      ease: "power4.out",
-      stagger: {
-        amount: 1,
-        ease: "power2.inOut",
-      },
-    });
-}, []);
+    gsap.timeline()
+      .set(".style-1 .char", { opacity: 0, y: 50 })
+      .to(".style-1 .char", {
+        y: 0,
+        opacity: 1,
+        duration: 1.8,
+        ease: "power4.out",
+        stagger: {
+          amount: 1,
+          ease: "power2.inOut",
+        },
+      });
+
+  }, []);
 
 
   const [scrolling, setScrolling] = useState(false);
@@ -94,7 +94,6 @@ const Banner = () => {
             autoPlay
             loop
             width="100%"
-            preload="none"
             id="myVideo">
             <source src="https://media.quecko.com/videos/bannervideo.mp4" type="video/mp4" />
           </video>
