59797aa9cfc4a4fe88361759329aea5fc9e73cbd
hamzach625 <hamzach625@gmail.com>
2025-10-15 00:17:50 +0500

    new changes

--- src/pages/component/Landing/marquee.js
+++ src/pages/component/Landing/marquee.js
@@ -6,9 +6,7 @@ const Marquee = () => {
   useEffect(() => {
     const fetchPrices = async () => {
       try {
-        const res = await fetch(
-          "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=50&page=1"
-        );
+        const res = await fetch("/api/prices");
         const data = await res.json();
         setCoins(data);
       } catch (err) {
